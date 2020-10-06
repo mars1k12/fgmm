adef3329130def91b6b504b5c195bff90c205042
Mir <mirlan.ru@inbox.ru>
2020-10-07 02:29:08 +0500

    changes

---
+++ js/parallax1.js
@@ -0,0 +1,33 @@
+//parallax
+(function() {
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector(".parallax1");
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.015}% ${50 - (_mouseY - _h) * 0.015}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        elem.style.backgroundPosition = x;
+    }
+
+})();
+(function() {
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector(".parallax2");
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.03}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.04}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.07}%`;
+        let x = `${_depth1}, ${_depth2}, ${_depth3}`;
+        elem.style.backgroundPosition = x;
+    }
+
+})();
