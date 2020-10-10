f1dbf105c27427fe70cec359c7dabbf6f3407ba0
Mir <mirlan.ru@inbox.ru>
2020-10-11 03:37:12 +0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 //открытие модального окна входа
 $('.login-link').on('click', function () {
     $('.login-block').addClass('active');
@@ -72,6 +70,7 @@ $('.bg-overlay').on('click', function () {
 //Закрытие модальных окон входа, регистрации, покупки
 $('.close-form').on('click', function () {
     $('.form-block').removeClass('active');  
+    $('.form-block').hide(); 
     $('.bg-overlay').removeClass('active');  
     $('body').removeClass('overflow-hidden');      
 });
@@ -87,5 +86,196 @@ $('.close-success-change').on('click', function () {
 
 
 
+
+
+//валидация регистрации
+$(document).ready(function () {
+    $("#regform").validate({
+        rules: {
+            "login": {
+                required: true,
+            },
+            "email": {
+                required: true,
+                email: true
+            },
+            "password": {
+                required: true
+            },
+            "password2": {                
+                equalTo : "#password"
+            },
+            "agree-chk": {
+                required: true,
+            },
+        },
+        messages: {
+            "login": {
+                required: "Введите логин"
+            },
+            "email": {
+                required: "Введите имя",
+                email: "Неверный формат почты"
+            },
+            "password": {
+                required: "Введите пароль"
+            },
+            "password2": {
+                required: "Введите пароль"
+            },
+            "agree-chk": {
+                required: "Нет согласия"
+            }
+        }
+    });
+});
+
+
+
+//валидация входа
+$(document).ready(function () {
+    $("#loginform").validate({
+        rules: {
+            "login": {
+                required: true,
+            },
+            "password": {
+                required: true
+            },
+
+        },
+        messages: {
+            "login": {
+                required: "Введите логин"
+            },
+            "password": {
+                required: "Введите пароль"
+            },
+        }
+    });
+});
+
+
+
+//валидация депозита
+$(document).ready(function () {
+    $("#depositform").validate({
+        rules: {
+            "sum": {
+                required: true,
+            },
+        },
+        messages: {
+            "sum": {
+                required: "Введите сумму",
+                min: "Некорректная сумма",
+                remote: "Некорректная сумма"
+            },
+        }
+    });
+});
+
+
+
+//валидация смены почты
+$(document).ready(function () {
+    $("#changemailform").validate({
+        rules: {
+            "email1": {
+                required: true,
+                email: true
+            },
+            "email2": {
+                required: true,
+                email: true
+            },
+            "email3": {
+                email: true
+            },
+        },
+        messages: {
+            "email1": {
+                required: "Введите почту",
+                email: "Неверный формат почты"
+            },
+            "email2": {
+                required: "Введите почту",
+                email: "Неверный формат почты"
+            },
+            "email3": {
+                required: "Введите имя",
+                email: "Неверный формат почты"
+            },
+        }
+    });
+});
+
+
+
+
+//валидация кода под-ния смены почты
+$(document).ready(function () {
+    $("#changemailform2").validate({
+        rules: {
+            "pass-code": {
+                required: true,
+            },
+        },
+        messages: {
+            "pass-code": {
+                required: "Введите код подтвержения",
+            },
+        }
+    });
+});
+
+
+//валидация смены пароля
+$(document).ready(function () {
+    $("#changemailform").validate({
+        rules: {
+            "password1": {
+                required: true,
+            },
+            "password2": {
+                required: true,
+            },
+            "password3": {
+
+            },
+        },
+        messages: {
+            "password1": {
+                required: "Введите почту",
+            },
+            "password2": {
+                required: "Введите почту",
+            },
+            "password3": {
+
+            },
+        }
+    });
+});
+
+
+
+
+
+//валидация смены пароля2
+$(document).ready(function () {
+    $("#changpassform2").validate({
+        rules: {
+            "pass-code": {
+                required: true,
+            },
+        },
+        messages: {
+            "pass-code": {
+                required: "Введите код подтвержения",
+            },
+        }
+    });
+});
 
 
