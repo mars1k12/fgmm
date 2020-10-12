fc12da12d2707512bb672187ba728f05fe9b1dd5
Mir <mirlan.ru@inbox.ru>
2020-10-12 22:01:19 +0500

    changes

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $('.reg-link').on('click', function () {
 //открытие модального окна входа при регистрации
 $('.login-link2').on('click', function () {
     $('.reg-block').removeClass('active');
-    $('.login-block').addClass('active');    
+    $('.login-block').addClass('active');
     $('.bg-overlay').addClass('active');
     $('body').addClass('overflow-hidden');
 });
@@ -27,7 +27,7 @@ $('.login-link2').on('click', function () {
 
 //открытие модального окна депозита
 $('.deposit-btn').on('click', function () {
-    $('.deposit-block').addClass('active'); 
+    $('.deposit-block').addClass('active');
     $('.bg-overlay').addClass('active');
     $('body').addClass('overflow-hidden');
 });
@@ -35,7 +35,7 @@ $('.deposit-btn').on('click', function () {
 
 //открытие модального окна скачивания файла
 $('.download-btn ').on('click', function () {
-    $('.download-block').addClass('active'); 
+    $('.download-block').addClass('active');
     $('.bg-overlay').addClass('active');
     $('body').addClass('overflow-hidden');
 });
@@ -43,7 +43,7 @@ $('.download-btn ').on('click', function () {
 
 //открытие модального окна изменения почты
 $('.change-email').on('click', function () {
-    $('.change-email-block').addClass('active');  
+    $('.change-email-block').addClass('active');
     $('.bg-overlay').addClass('active');
     $('body').addClass('overflow-hidden');
 });
@@ -51,7 +51,7 @@ $('.change-email').on('click', function () {
 
 //открытие модального окна изменения пароля
 $('.change-pass').on('click', function () {
-    $('.change-pass-block').addClass('active');  
+    $('.change-pass-block').addClass('active');
     $('.bg-overlay').addClass('active');
     $('body').addClass('overflow-hidden');
 });
@@ -61,18 +61,17 @@ $('.change-pass').on('click', function () {
 
 //Закрытие модальных окон входа, регистрации, покупки
 $('.bg-overlay').on('click', function () {
-    $('.form-block').removeClass('active');  
-    $('.bg-overlay').removeClass('active');  
-    $('body').removeClass('overflow-hidden');   
+    $('.form-block').removeClass('active');
+    $('.bg-overlay').removeClass('active');
+    $('body').removeClass('overflow-hidden');
 });
 
 
 //Закрытие модальных окон входа, регистрации, покупки
 $('.close-form').on('click', function () {
-    $('.form-block').removeClass('active');  
-    $('.form-block').hide(); 
-    $('.bg-overlay').removeClass('active');  
-    $('body').removeClass('overflow-hidden');      
+    $('.form-block').removeClass('active');
+    $('.bg-overlay').removeClass('active');
+    $('body').removeClass('overflow-hidden');
 });
 
 
@@ -102,8 +101,8 @@ $(document).ready(function () {
             "password": {
                 required: true
             },
-            "password2": {                
-                equalTo : "#password"
+            "password2": {
+                equalTo: "#password"
             },
             "agree-chk": {
                 required: true,
@@ -279,3 +278,72 @@ $(document).ready(function () {
 });
 
 
+
+
+//carousel
+$(document).ready(function () {
+    $('#owl-first').owlCarousel({
+        margin: 25,
+        item: 2,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 3500,
+        autoplayHoverPause: true,
+        slideTransition: 'linear',
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1,
+                nav: false,
+                margin: 50
+            },
+            2000: {
+                items: 2
+            }
+        }
+    })
+    $('#owl-second').owlCarousel({
+        margin: 25,
+        item: 2,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 3500,
+        autoplayHoverPause: true,
+        slideTransition: 'linear',
+        nav: false,
+        rtl:true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1,
+                nav: false,
+                margin: 50
+            },
+            2000: {
+                items: 2
+            }
+        }
+    })
+});
+
+
+
+
+
+$(".owl-carousel img").click(function(){
+    $("#full-image").attr("src", $(this).attr("src"));
+    $('#image-viewer').show();
+  });
+  
+  $("#image-viewer .close").click(function(){
+    $('#image-viewer').hide();
+  });
